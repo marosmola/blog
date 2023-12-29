22ffcba2dcabce02414d8408bb29d22bcf929725
marosmola <ssmolenm@gmail.com>
2023-12-29 15:25:24 +0100

    Create blog structure, add tailwindcss

---
+++ app/blog/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mt-24 mx-auto max-w-2xl">
+    <h1
+      class="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl"
+    >
+      I’m Spencer Sharp. I live in New York City, where I design the future.
+    </h1>
+    <div class="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+      <p>
+        I’ve loved making things for as long as I can remember, and wrote my
+        first program when I was 6 years old, just two weeks after my mom
+        brought home the brand new Macintosh LC 550 that I taught myself to type
+        on.
+      </p>
+      <p>
+        The only thing I loved more than computers as a kid was space. When I
+        was 8, I climbed the 40-foot oak tree at the back of our yard while
+        wearing my older sister’s motorcycle helmet, counted down from three,
+        and jumped — hoping the tree was tall enough that with just a bit of
+        momentum I’d be able to get to orbit.
+      </p>
+    </div>
+  </div>
+</template>
